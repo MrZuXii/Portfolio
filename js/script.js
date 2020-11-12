4959fc3041ec01a87b2e2f0493d8aa7f413bfb05
Kamil Bieniek <mrzuxii1337@gmail.com>
2020-11-12 02:52:32 +0100

    Update #2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector(".burger");
+
+const column = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+burger.addEventListener("click", function () {
+    column.classList.toggle("show"); //nie
+})
