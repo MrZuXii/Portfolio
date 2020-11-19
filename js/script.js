d03f94dbfa65f396ac3fdcddfb0ee1751270e20a
Kamil Bieniek <mrzuxii1337@gmail.com>
2020-11-19 04:55:08 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const IconBurger = document.querySelector(".burger");
 const HeaderNav = document.querySelector("nav");
-IconBurger.addEventListener("click", function () {
-    HeaderNav.classList.toggle("shows"); //nie
-})
+const OverFlow = document.querySelectorAll(".overflow")
+const Cross = document.querySelector(".fa-times")
+
+function ChangeMenu() {
+    HeaderNav.classList.toggle("shows");
+    OverFlow[0].classList.toggle("on");
+    OverFlow[1].classList.toggle("on");
+    document.body.classList.toggle("on");
+}
+IconBurger.addEventListener("click", ChangeMenu)
+IconBurger.addEventListener("click", ChangeMenu)
+OverFlow[0].addEventListener("click", ChangeMenu)
+OverFlow[1].addEventListener("click", ChangeMenu)
+Cross.addEventListener("click", ChangeMenu)
