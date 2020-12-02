d17fb46ce8aa1d05e175fee6aa253fdf220509c8
Kamil Bieniek <mrzuxii1337@gmail.com>
2020-12-02 03:00:26 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,7 @@
-const IconBurger = document.querySelector(".burger");
-IconBurger.addEventListener("click", ChangeMenu)
-
-const HeaderNav = document.querySelector("nav");
-
-const OverFlow = document.querySelectorAll(".overflow")
-OverFlow[0].addEventListener("click", ChangeMenu)
-OverFlow[1].addEventListener("click", ChangeMenu)
-
-const MenuLink = document.querySelectorAll(".menu a")
-
-MenuLink[0].addEventListener("click", ChangeMenu)
-MenuLink[1].addEventListener("click", ChangeMenu)
-MenuLink[2].addEventListener("click", ChangeMenu)
-MenuLink[3].addEventListener("click", ChangeMenu)
+const MenuLinks = document.querySelectorAll(".menu a, .overflow, .burger")
+MenuLinks.forEach((MenuLink) => MenuLink.addEventListener("click", ChangeMenu))
 
 function ChangeMenu() {
-    HeaderNav.classList.toggle("shows");
-    OverFlow[0].classList.toggle("on");
-    OverFlow[1].classList.toggle("on");
-    document.body.classList.toggle("on");
-    IconBurger.classList.toggle("on");
+    document.querySelector("nav").classList.toggle("on");
+    MenuLinks.forEach((OverFlow) => OverFlow.classList.toggle("on"));
 }
