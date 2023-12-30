37a697ec1801e4a7ddeac9e54f7e81db11a3bb37
MrZuXii <kamilb231@op.pl>
2023-12-30 23:41:09 +0100

    Adding script to update age and copyright year

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-const MenuLinks = document.querySelectorAll(".menu a, .overflow, .burger")
+const MenuLinks = document.querySelectorAll(".menu a, .overflow, .burger");
 const BtnTop = document.querySelector("div.top");
-MenuLinks.forEach((MenuLink) => MenuLink.addEventListener("click", ChangeMenu))
+const age = document.querySelector(".age");
+const year = document.querySelector(".year");
+MenuLinks.forEach((MenuLink) => MenuLink.addEventListener("click", ChangeMenu));
 
 function ChangeMenu() {
-    document.querySelector("nav").classList.toggle("on");
-    MenuLinks.forEach((OverFlow) => OverFlow.classList.toggle("on"));
+  document.querySelector("nav").classList.toggle("on");
+  MenuLinks.forEach((OverFlow) => OverFlow.classList.toggle("on"));
 }
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 300)
-        BtnTop.classList.add("on")
-    else
-        BtnTop.classList.remove("on")
-})
+  if (window.scrollY > 300) BtnTop.classList.add("on");
+  else BtnTop.classList.remove("on");
+});
 BtnTop.addEventListener("click", function () {
-    window.scrollTo(0, 0);
-})
+  window.scrollTo(0, 0);
+});
+
+year.textContent = new Date().getFullYear();
+const ageCalc =
+  new Date().getMonth() > 4 ||
+  (new Date().getMonth() == 4 && new Date().getDay >= 4);
+
+age.textContent = ageCalc
+  ? new Date().getFullYear() - 2000
+  : new Date().getFullYear() - 2001;
+console.log(new Date().getMonth());
+
+console.log();
